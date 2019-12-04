a6d0850f8b8dbc6bb417963c417533c3da2d0c34
n4rd0 <58040410+n4rd0@users.noreply.github.com>
2019-12-04 22:56:56 +0100

    Update D3.cpp

--- D3/D3.cpp
+++ D3/D3.cpp
@@ -7,12 +7,11 @@
 
 using namespace std;
 
-struct point{
-int x;
-int y;
-int step;
+struct point {
+	int x;
+	int y;
+	int step;
 };
-
 std::vector<int> parseNums(string name) {
 	std::vector<int> numbers;
 	ifstream myFile;
@@ -33,7 +32,6 @@ std::vector<int> parseNums(string name) {
 
 	return numbers;
 }
-
 std::vector<char> parseLeters(string name) {
 	std::vector<char> letters;
 	ifstream myFile;
@@ -61,6 +59,7 @@ std::vector<point> D31_1(std::vector<char> leters, std::vector<int>numbers) {
 	for (int i = 0; i < numbers.size(); i++) {
 		int n = numbers[i];
 		while (n > 0) {
+
 			switch (leters[i]) {
 			case 'U': lastPoint.y++; lastPoint.step++; break;
 			case 'D': lastPoint.y--; lastPoint.step++; break;
@@ -73,12 +72,12 @@ std::vector<point> D31_1(std::vector<char> leters, std::vector<int>numbers) {
 			n--;
 		}
 	}
-	
+
 	return points;
 }
-
 std::vector<point> D31_2(vector<point> wire1, vector<point> wire2) {
 	std::vector<point> repeated;
+	int min_step = 1233412341234123;
 
 	for (int i = 0; i < wire1.size(); i++) {
 		point search = wire1[i];
@@ -89,37 +88,28 @@ std::vector<point> D31_2(vector<point> wire1, vector<point> wire2) {
 
 			if (x == search.x && y == search.y) {
 				repeated.push_back(search);
-				search.step += wire2[j].step;
-				cout << search.x << " " << search.y<<" "<<search.step<< endl;
+				if (min_step > (search.step + wire2[j].step)) min_step = search.step + wire2[j].step;
+
+				cout << search.x << " " << search.y << " " << search.step+wire2[j].step << endl;
 			}
 		}
 	}
 
+	cout << "Answer 2 = " << min_step << endl;
 	return repeated;
 }
-
 point D31_3(vector<point> repeated) {
 	point res = repeated[0];
 
 	for (int i = 1; i < repeated.size(); i++) {
-		if ((abs(res.x) + abs(res.y)) > (abs(repeated[i].x) + abs(repeated[i].y))) res = repeated[i]; cout << res.x<<" " << endl;
+		if ((abs(res.x) + abs(res.y)) > (abs(repeated[i].x) + abs(repeated[i].y))) res = repeated[i]; 
 	}
 
 	return res;
 }
 
-int D32_1(vector<point> repeated) {
-	point res = repeated[0];
-	int minStep = res.step;
-
-	for (int i = 1; i < repeated.size(); i++) {
-		if ((repeated[i].step) < (minStep)) minStep = repeated[i].step; cout << res.step << endl;
-	}
-
-	return minStep;
-}
-
 int main() {
+
 	std::vector <char> letersWire1 = parseLeters("wire1.txt");
 	std::vector <char> letersWire2 = parseLeters("wire2.txt");
 
@@ -130,8 +120,6 @@ int main() {
 	std::vector<point> pointsWire2 = D31_1(letersWire2, numsWire2);
 	std::vector<point> repeatedPoints = D31_2(pointsWire1, pointsWire2);
 	point answer = D31_3(repeatedPoints);
-	int answer2= D32_1(repeatedPoints);
 
-	cout << "Answer 1 = "<<abs(answer.x) + abs(answer.y) << endl;
-	cout << "Answer 2 = " << answer2 << endl;
+	cout << "Answer 1 = " << abs(answer.x) + abs(answer.y) << endl;
 }
