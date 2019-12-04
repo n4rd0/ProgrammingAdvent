e7a9036dcfd14046a74f991be8d31f0589cbafc3
n4rd0 <58040410+n4rd0@users.noreply.github.com>
2019-12-04 21:19:37 +0100

    Update D4.cpp

--- D4/D4.cpp
+++ D4/D4.cpp
@@ -1,11 +1,4 @@
-// D4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include <map>
-#include <fstream>
-#include <cstdlib>
-#include <string>
 #include <vector>
 
 using namespace std;
