c862e18986b1441ceff775d7d6ac24d4f46b690e
lbm364dl <geometrydashm364@gmail.com>
2020-12-03 13:38:15 +0100

    lbm D3 python

---
+++ AoC2020/D3/lbm.py
@@ -0,0 +1,17 @@
+from functools import reduce
+from math import ceil
+
+def sumTrees(dx, dy, inp):
+	return sum(1 if inp[dy*i][(dx*i)%len(inp[0])]=='#' else 0 for i in range(ceil(len(inp)/dy)))
+
+def star1(inp):
+	return sumTrees(3, 1, inp)
+
+def star2(inp):
+	return reduce(lambda x,y:x*y, [sumTrees(dx, dy, inp) for (dx, dy) in [(1,1),(3,1),(5,1),(7,1),(1,2)] ])
+
+with open("input3.txt") as f:
+	inp = f.read().splitlines()
+
+print("Star 1:", star1(inp))	
+print("Star 2:", star2(inp))
