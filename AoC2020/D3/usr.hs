7daaeb48081cbf0617635143ed63b3a76ad34f5a
usr25 <jorgespaguettini@gmail.com>
2020-12-03 13:41:55 +0100

    D3 improvements

--- AoC2020/D3/usr.hs
+++ AoC2020/D3/usr.hs
@@ -1,15 +1,15 @@
 
-star1 :: [[Bool]] -> (Int,Int) -> Int
+star1 :: [[Int]] -> (Int,Int) -> Int
 star1 ls (x,y) = helper ls 1 0
     where
         len = (length . head) ls
-        helper :: [[Bool]] -> Int -> Int -> Int
+        helper :: [[Int]] -> Int -> Int -> Int
         helper ls' t acc 
             | length ls' <= y = acc
             | otherwise = helper (k:ks) (t+1) acc'
             where
                 (k:ks) = drop y ls'
-                acc' = if k !! ((x*t) `mod` len) then 1 + acc else acc
+                acc' = acc + (k !! ((x*t) `mod` len))
 
 directions = [
     (1,1),
@@ -19,12 +19,12 @@ directions = [
     (1,2)
     ]
 
-star2 :: [[Bool]] -> Int
+star2 :: [[Int]] -> Int
 star2 ls = (product . map (star1 ls)) directions
 
 main :: IO ()
 main = do
   contents <- getContents
-  let ls = map (map (=='#')) $ lines contents
+  let ls = map (map (\x -> if x=='#' then 1 else 0)) $ lines contents
   putStrLn $ "Star 1: " ++ (show $ star1 ls (3,1))
   putStrLn $ "Star 2: " ++ (show $ star2 ls)
