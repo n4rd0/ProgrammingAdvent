b719b79f9f3fcfbab6da30bbe4204d8d4013a91c
lbm364dl <geometrydashm364@gmail.com>
2020-12-05 18:49:42 +0100

    changes lbm D5 python

--- AoC2020/D5/lbm.py
+++ AoC2020/D5/lbm.py
@@ -1,20 +1,7 @@
-def parseInput(file):
-	toBinary = {"B" : "1", "F" : "0", "R" : "1", "L" : "0"}
-	with open(file) as f:
-		inp = [ int("".join([toBinary[c] for c in line]), 2) for line in f.read().splitlines()]
+toBinary = {'B':'1', 'F':'0', 'R':'1', 'L':'0'}
 
-	return inp
+with open("input5.txt") as f:
+	inp = [ int("".join([toBinary[c] for c in line]), 2) for line in f.read().splitlines()]
 
-def star1(inp):
-	return inp[-1]
-
-def star2(inp, star):
-	if inp[0] != star:
-		return star
-	
-	return star2(inp[1:], star + 1)
-
-inp = parseInput("input5.txt")
-inp.sort()
-print("Star 1:", star1(inp))
-print("Star 2:", star2(inp, inp[0]))
+print("Star 1:", max(inp))
+print("Star 2:", sum(range(min(inp), max(inp)+1)) - sum(inp))
