bf873e51e36e4955df119a03e9fbdc85e1c93368
n4rd0 <58040410+n4rd0@users.noreply.github.com>
2020-12-05 12:12:18 +0100

    Create n4rd0.py

---
+++ AoC2020/D4/n4rd0.py
@@ -0,0 +1,106 @@
+#parse
+file = "D4.txt"
+passports = []
+valid_passports = []
+line = " "
+accum = " "
+
+with open(file,"r") as f:
+
+	while line != "":
+
+		line = f.readline()
+
+		if line == "\n":
+			passports.append(accum)
+			accum = ""
+
+		else:
+			accum = accum+line
+
+def fstStar(list):
+
+	for i in range(len(list)):
+
+		temp_text = list[i]
+		nice_passport = "byr" in temp_text and "iyr" in temp_text and "eyr" in temp_text and "hgt" in temp_text and "hcl" in temp_text and "ecl" in temp_text and "pid" in temp_text
+
+		if nice_passport:
+			valid_passports.append(list[i])	
+
+	return len(valid_passports)
+
+def scndStar(list):
+	
+	counter = 0
+	res = 0
+
+	for i in range(len(valid_passports)):
+
+		for j in range(len(valid_passports[i])):
+			
+			if valid_passports[i][j:j+3] == "byr":
+				birth_year = int(valid_passports[i][j+4:j+8])
+				
+				if 1920<=birth_year<=2002:
+					counter = counter+1
+					
+			if valid_passports[i][j:j+3] == "iyr":
+				issue_year = int(valid_passports[i][j+4:j+8])
+
+				if 2010<=issue_year<=2020:
+					counter = counter+1
+					
+			if valid_passports[i][j:j+3] == "eyr":
+				expiration_year = int(valid_passports[i][j+4:j+8])
+
+				if 2020<=expiration_year<=2030:
+					counter = counter+1
+
+			if valid_passports[i][j:j+3] == "hgt":
+				height = valid_passports[i][j+4:j+9].strip()
+
+				if "cm" in height and len(height) == 5:
+					if 150<=int(height[0:3])<= 193:
+						
+						counter = counter +1
+
+				if "in" in height and len(height) == 4: 
+					if 59<=int(height[0:2]) <= 76:
+
+						counter = counter +1
+
+			if valid_passports[i][j:j+3] == "hcl":
+				hair_colour = valid_passports[i][j+4:j+11]
+				
+				if hair_colour[0] == "#" and len(hair_colour) == 7:
+					counter = counter+1
+			
+			if valid_passports[i][j:j+3] == "ecl":
+				eye_colour = valid_passports[i][j+4:j+7]
+
+				if eye_colour == "amb" or eye_colour == "blu" or eye_colour == "brn" or eye_colour == "gry" or eye_colour == "grn" or eye_colour == "hzl" or eye_colour == "oth":
+
+					counter = counter+1
+
+			if valid_passports[i][j:j+3] == "pid":
+				counter_id = 0
+				passport_id = valid_passports[i][j+4:j+14].strip()
+
+				for k in range(len(passport_id)):
+
+					if passport_id[k] != "0" and passport_id[k] != "1" and passport_id[k] != "2" and passport_id[k] != "3" and passport_id[k] != "4" and passport_id[k] != "5" and passport_id[k] != "6" and passport_id[k] != "7" and passport_id[k] != "8" and passport_id[k] != "9":
+						counter_id = counter_id
+					else:
+						counter_id = counter_id +1
+
+				if counter_id == 9 : 
+					counter = counter +1
+
+		if counter == 7:
+			res = res + 1
+		counter = 0
+	return res
+
+print("First Star Answer = ",fstStar(passports))
+print("Second Star Answer =",scndStar(passports))
