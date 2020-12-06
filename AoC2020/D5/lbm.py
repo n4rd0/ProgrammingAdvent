e9ee2bcb1e507c7455533ff43473b28f18ade0f3
lbm364dl <geometrydashm364@gmail.com>
2020-12-06 09:39:19 +0100

    deleted unepic space lbm D5 python

--- AoC2020/D5/lbm.py
+++ AoC2020/D5/lbm.py
@@ -1,7 +1,7 @@
 toBinary = {'B':'1', 'F':'0', 'R':'1', 'L':'0'}
 
 with open("input5.txt") as f:
-	inp = [ int("".join([toBinary[c] for c in line]), 2) for line in f.read().splitlines()]
+	inp = [int("".join([toBinary[c] for c in line]), 2) for line in f.read().splitlines()]
 
 print("Star 1:", max(inp))
 print("Star 2:", sum(range(min(inp), max(inp)+1)) - sum(inp))
