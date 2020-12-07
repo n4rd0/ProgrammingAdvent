d799b28a01aaf2fbeb06867399dc62900234d98a
lbm364dl <geometrydashm364@gmail.com>
2020-12-07 11:53:06 +0100

    lbm D7 python added two comments

--- AoC2020/D7/lbm.py
+++ AoC2020/D7/lbm.py
@@ -5,9 +5,10 @@ def parseInp(inp):
 	bags = dict()
 
 	for bagType in inp:
+		#e.g. ['muted aqua', '4 wavy bronze', ' 1 pale plum']
 		outerBag = bagType[0]
 		bags[outerBag] = dict()
-		#if not "no other bags"
+		#e.g. ['clear olive', 'no other']
 		if bagType[1][0] != 'n':
 			bags[outerBag][bagType[1][2:]] = int(bagType[1][0])
 
@@ -30,14 +31,14 @@ def checkShinyGold(bags, currentBag, entries):
 		return False
 
 	return 	checkShinyGold(bags, entries[0], list(bags[entries[0]])) or \
-			checkShinyGold(bags, currentBag, entries[1:])
+	checkShinyGold(bags, currentBag, entries[1:])
 
 def star2(bags, currentBag, entries):
 	if not (bags[currentBag] and entries):
 		return 0
 
 	return 	bags[currentBag][entries[0]] * (1 + star2(bags, entries[0], list(bags[entries[0]]))) + \
-			star2(bags, currentBag, entries[1:])
+	star2(bags, currentBag, entries[1:])
 
 with open("input7.txt") as f:
 	bags = parseInp(f.read().splitlines())
