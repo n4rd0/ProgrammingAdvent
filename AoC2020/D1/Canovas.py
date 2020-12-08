ac42bd15e9275f8c807c8fd40889c28f4c69ced6
C4novas <75586610+C4novas@users.noreply.github.com>
2020-12-08 18:55:13 +0100

    jaja salu2

--- AoC2020/D1/Canovas.py
+++ AoC2020/D1/Canovas.py
@@ -8,14 +8,13 @@ def oddEven(numberList):
 	return even,odd
 
 def firstStar(numberList):
-	numberList_copy = numberList[:]
-	even,odd = oddEven(numberList_copy)
+	even,odd = oddEven(numberList)
 	good_ending = False
 
-	while(not good_ending and len(numberList_copy) > 0 ):
-		number_1 = numberList_copy.pop()
+	while not good_ending and len(numberList) > 0 :
+		number_1 = numberList.pop()
 		for number_2 in (even if number_1%2 == 0 else odd):
-			if(number_1 + number_2 == 2020):
+			if number_1 + number_2 == 2020:
 				good_ending = True
 				result = number_1 * number_2
 
@@ -23,15 +22,14 @@ def firstStar(numberList):
 
 
 def secondStar(numberList):
-	numberList_copy = numberList[:]
-	even, odd = oddEven(numberList_copy)
+	even, odd = oddEven(numberList)
 	good_ending = False
 
-	while(not good_ending and len(numberList_copy) > 0):
-		number_1 = numberList_copy.pop()
+	while not good_ending and len(numberList) > 0:
+		number_1 = numberList.pop()
 		for number_2 in even:
 			sum_12 = number_1 + number_2
-			if(sum_12 < 2020):
+			if sum_12 < 2020:
 				for number_3 in (even if sum_12%2 == 0 else odd):
 					if sum_12 + number_3 == 2020:
 						good_ending = True
@@ -43,5 +41,5 @@ def secondStar(numberList):
 with open('input.txt') as file:
 	input_list = [int(i) for i in file.readlines()]		
 
-print(firstStar(input_list))
-print(secondStar(input_list))
+print(firstStar(list(input_list)))
+print(secondStar(list(input_list)))
