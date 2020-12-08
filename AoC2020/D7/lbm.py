88c1f0a6e8838283559e5ba87a72253ebd163006
lbm364dl <geometrydashm364@gmail.com>
2020-12-08 18:13:52 +0100

    lbm improved parse function D7 python

--- AoC2020/D7/lbm.py
+++ AoC2020/D7/lbm.py
@@ -1,21 +1,10 @@
 import re
 
 def parseInp(inp):
-	inp = [re.split(r" bags contain | bag[s]{0,1}[,]{0,1}", bagType[:-1])[:-1] for bagType in inp]
-	bags = dict()
-
-	for bagType in inp:
-		#e.g. ['muted aqua', '4 wavy bronze', ' 1 pale plum']
-		outerBag = bagType[0]
-		bags[outerBag] = dict()
-		#e.g. ['clear olive', 'no other']
-		if bagType[1][0] != 'n':
-			bags[outerBag][bagType[1][2:]] = int(bagType[1][0])
-
-		for bagsInside in bagType[2:]:
-			bags[outerBag][bagsInside[3:]] = int(bagsInside[1])
-
-	return bags
+	bagTypes = re.findall(r"(.+?)(?= bags contain)",inp)
+	bagsInside = [dict(map(lambda p:(p[1], int(p[0])), re.findall(r"(\d+) (.+?) bags?[,.]", bagType))) \
+					for bagType in inp.splitlines()]
+	return dict(zip(bagTypes, bagsInside))
 
 def star1(bags, entries):
 	if not entries:
@@ -31,17 +20,17 @@ def checkShinyGold(bags, currentBag, entries):
 		return False
 
 	return 	checkShinyGold(bags, entries[0], list(bags[entries[0]])) or \
-	checkShinyGold(bags, currentBag, entries[1:])
+				checkShinyGold(bags, currentBag, entries[1:])
 
 def star2(bags, currentBag, entries):
 	if not (bags[currentBag] and entries):
 		return 0
 
 	return 	bags[currentBag][entries[0]] * (1 + star2(bags, entries[0], list(bags[entries[0]]))) + \
-	star2(bags, currentBag, entries[1:])
+				star2(bags, currentBag, entries[1:])
 
 with open("input7.txt") as f:
-	bags = parseInp(f.read().splitlines())
+	bags = parseInp(f.read())
 
 print("Star 1:", star1(bags, list(bags)))
-print("Star 2:", star2(bags, "shiny gold", list(bags["shiny gold"])))
+print("Star 2:", star2(bags, "shiny gold", list(bags["shiny gold"])))	
