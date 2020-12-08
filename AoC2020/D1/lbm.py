5b20b6e927c1a8ceaa651e476108f73527a79f11
lbm364dl <geometrydashm364@gmail.com>
2020-12-08 19:06:59 +0100

    lbm shorter code D1 python

--- AoC2020/D1/lbm.py
+++ AoC2020/D1/lbm.py
@@ -1,23 +1,5 @@
-import fileinput
+with open("input1.txt") as f:
+	inp = list(map(int, f.read().splitlines()))
 
-def star1(inp):
-	for i in range(len(inp)-1):
-		last = inp.pop()
-		for num in inp:
-			if num + last == 2020:
-				return num*last
-
-def star2(inp):
-	for i in range(len(inp)-2, 0, -1):
-		last = inp.pop()
-		for j in range(i, 0, -1):
-			other = inp[j]
-			for num in inp[:j]:
-				if num + other + last == 2020:
-					return num*last*other
-
-
-inp = [int(line) for line in fileinput.input()]
-	
-print("Star 1:",star1(inp))
-print("Star 2:",star2(inp))
+print("Star 1:", [num*(2020-num) for num in inp if num!=1010 and 2020-num in inp][0])
+print("Star 2:", [n1*n2*(2020-n1-n2) for n1 in inp for n2 in inp if n1!=n2 and n1!=(2020-n1-n2) and 2020-n1-n2 in inp][0])
