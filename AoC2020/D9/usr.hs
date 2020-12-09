570775383d41b5f245f193cd911f53fe3d74bb53
usr25 <jorgespaguettini@gmail.com>
2020-12-09 14:02:32 +0100

    Cleaner code

--- AoC2020/D9/usr.hs
+++ AoC2020/D9/usr.hs
@@ -1,26 +1,20 @@
-import Data.Maybe (fromJust)
-
 sums :: [Int] -> [Int]
 sums (l:ls) = map (+l) ls ++ sums ls
 sums _ = []
 
 findRange :: Int -> [Int] -> [Int] -> Int -> [Int] -> [Int]
-findRange target nss@(n:ns) (r:rest) s range = if s' == target
+findRange target nss@(_:ns) (r:rest) s range = if s' == target
     then r:range
     else if s' < target
         then findRange target nss rest s' (r:range)
         else findRange target ns ns 0 []
-        where
-            s' = s + r
+        where s' = s + r
 findRange _ [] _ _ _ = error "Can't find range"
-
---Lists are better than sets due to the sizes used here
 
 star1 nums
     | length nums <= preamble = error "Conditions for star 1 aren't met"
     | otherwise = if not (a `elem` sums (b:bef)) then a else star1 (bef++a:after)
-    where
-        (b:bef,a:after) = splitAt preamble nums
+    where (b:bef,a:after) = splitAt preamble nums
 
 star2 nums = minimum range + maximum range
     where range = findRange (star1 nums) nums nums 0 []
