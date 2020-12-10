533dfdce36d2bdb5d6b59760f5259589d20e87f1
lbm364dl <geometrydashm364@gmail.com>
2020-12-10 20:50:08 +0100

    improved D10 python

--- AoC2020/D10/lbm.py
+++ AoC2020/D10/lbm.py
@@ -1,36 +1,28 @@
+from functools import reduce
 # number of partitions of n with elements at most 3 is (n+3)th tribonacci number 
 # 0, 0, 1, 1, 2, 4, 7, 13, 24, 44...
 # 1 -> 1 (1)
 # 2 -> 2 (1+1, 2)
 # 3 -> 4 (1+2, 2+1, 1+1+1, 3)
 # 4 -> 7 (1+2+1, 2+1+1, 1+1+1+1, 3+1, 1+1+2, 2+2, 1+3)
-def tribonacci(n):
-	if n==1 or n==2:
-		return 0
-	elif n==3:
-		return 1
-	return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3)
+def tribonacciUpTo(nth):
+	tribo = [0,0,1]
+
+	if nth <= 2:
+		return tribo[nth]
+
+	for i in range(3, nth+1):
+		tribo.append(tribo[i-1]+tribo[i-2]+tribo[i-3])
+
+	return tribo
 
 def star1(diffs):
-	counter = [0,0]
-	for num in diffs:
-		if num == 1:
-			counter[0] += 1
-		else:
-			counter[1] += 1
-	return counter[0]*counter[1]
+	ones, threes = reduce(lambda acc, x: [acc[0]+1, acc[1]] if x == 1 else [acc[0], acc[1]+1], diffs, [0,0])
+	return ones*threes
 
 def star2(diffs):
-	tribo = [tribonacci(n) for n in range(3,21)]
-	counter = 1
-	ones = 0
-	for num in diffs:
-		if num == 1:
-			ones += 1
-		else:
-			counter *= tribo[ones]
-			ones = 0
-	return counter
+	tribo = tribonacciUpTo(6)[2:]
+	return reduce(lambda acc, x: [acc[0], acc[1]+1] if x == 1 else [acc[0]*tribo[acc[1]], 0] , diffs, [1,0])[0]
 
 with open("input10.txt") as f:
 	inp = sorted(list(map(int, f.read().splitlines())))
