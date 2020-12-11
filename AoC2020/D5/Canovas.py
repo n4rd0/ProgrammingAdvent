df1079faea5402bc40e3fff9b94262016b19c871
C4novas <75586610+C4novas@users.noreply.github.com>
2020-12-11 17:29:01 +0100

    jaja salu2

--- AoC2020/D5/Canovas.py
+++ AoC2020/D5/Canovas.py
@@ -6,13 +6,15 @@ def firstStar(list_seats):
 	return int(re.match(r"\d{7}",maxSeat).group(),2) * 8 + int(re.search(r"\d{3}$",maxSeat).group(),2)
 
 def secondStar(list_seats):
-	list_seats_b10 = [int(i,2) for i in list_seats[:-1]]
+	list_seats_b10 = [int(i,2) for i in list_seats]
 
 	return sum(range(min(list_seats_b10),max(list_seats_b10)+1)) - sum(list_seats_b10)
 
 
+
+
 with open('input.txt') as file:
-	input_list = file.read()
+	input_list = file.read().strip("\n")
 
 
 input_list = input_list.replace("B","1")
