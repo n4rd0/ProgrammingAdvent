93f04dbb8c1d8b1d05edc3f641da09aed915a812
lbm364dl <geometrydashm364@gmail.com>
2020-12-12 10:40:37 +0100

    added Manhattan function D12 python

--- AoC2020/D12/lbm.py
+++ AoC2020/D12/lbm.py
@@ -15,10 +15,13 @@ def updateShips(acc, x):
 		
 	return [ship1, ship2, currentOrientation, wayPoint]
 	
+def manhattan(cmplx):
+	return int(abs(cmplx.real) + abs(cmplx.imag))
+
 with open("input12.txt") as f:
 	inp = [(instr[0], int(instr[1:]) if instr[0]!='L' and instr[0]!='R' else int(instr[1:])//90) for instr in f.read().splitlines()]
 
 dirs = {'N': 0 + 1j, 'E': 1 + 0j, 'S': 0 - 1j, 'W': -1 + 0j}
 ship1, ship2, _, _ = reduce(updateShips, inp, [0+0j, 0+0j, 1+0j, 10+1j])
-print("Star 1:", int(abs(ship1.real)+abs(ship1.imag)))
-print("Star 2:", int(abs(ship2.real)+abs(ship2.imag)))
+print("Star 1:", manhattan(ship1))
+print("Star 2:", manhattan(ship2))
