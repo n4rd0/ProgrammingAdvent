ad054a1f31582804f77d32a87d79348c5b11c6a9
lbm364dl <geometrydashm364@gmail.com>
2020-12-12 13:05:54 +0100

    improved performance D11 python

--- AoC2020/D11/lbm.py
+++ AoC2020/D11/lbm.py
@@ -1,74 +1,50 @@
-import copy
+def star1(last, square, wasChange, dirs):
+	if last[square] == free and all(True if last[square+delta] != full else False for delta in dirs):
+		return True, full  
+	elif last[square] == full and sum(1 for delta in dirs if last[square+delta] == full) >= 4:
+		return True, free
 
-def star1(inp, nonFloorSquares, dirs):
-	res = copy.deepcopy(inp)
-	wasChange = True
-	while wasChange:
-		wasChange = False
-		last = copy.deepcopy(res)
-		for nonFloorSq in nonFloorSquares:
-			row, col = nonFloorSq[0], nonFloorSq[1]
-			square = last[row][col]
-			if square == free and all(True if not last[row+delta[1]][col+delta[0]] == full else False for delta in dirs):
-				wasChange = True
-				res[row][col] = full
-			elif square == full and sum(1 for delta in dirs if last[row+delta[1]][col+delta[0]] == full) >= 4:
-				wasChange = True
-				res[row][col] = free
+	return wasChange, last[square]
 
-	return sum(1 for row in res[1:-1] for col in row[1:-1] if col == full)
-
-
-def star2(inp, nonFloorSquares, dirs):
-	res = copy.deepcopy(inp)
-	wasChange = True
-	while wasChange:
-		wasChange = False
-		last = copy.deepcopy(res)
-		for nonFloorSq in nonFloorSquares:
-			row, col = nonFloorSq[0], nonFloorSq[1]
-			square = last[row][col]
-			numFull = search(last, row, col, dirs)
-			if square == free and numFull == 0:
-				wasChange = True
-				res[row][col] = full
-			elif square == full and numFull >= 5:
-				wasChange = True
-				res[row][col] = free
-
-	return sum(1 for row in res[1:-1] for col in row[1:-1] if col == full)
-
-def search(inp, rowStart, colStart, dirs):
-	counter = 0
+def star2(last, square, wasChange, dirs):
+	numFulls = 0
 	for delta in dirs:
-		lastRow, lastCol = rowStart+delta[1], colStart+delta[0]
-		while inp[lastRow][lastCol] == floor and 1 <= lastRow <= rows and 1 <= lastCol <= cols:
-			lastRow, lastCol = lastRow+delta[1], lastCol+delta[0]
-		if inp[lastRow][lastCol] == full:
-			counter += 1 
+		currentSquare = square + delta 
+		while last[currentSquare] == floor and 1 <= currentSquare & 127 <= lastCol and 1 <= currentSquare >> 7 <= lastRow:
+			currentSquare += delta
+		if last[currentSquare] == full:
+			numFulls += 1
 
-	return counter	
+	if last[square] == free and numFulls == 0:
+		return True, full 
+	elif last[square] == full and numFulls >= 5:
+		return True, free
 
+	return wasChange, last[square]
+
+def star(inp, option, dirs):
+	opt = star1 if option == 1 else star2
+	res = inp[:]
+	wasChange = True
+	while wasChange:
+		wasChange = False
+		last = res[:]
+		for square in nonFloor:
+			wasChange, res[square] = opt(last, square, wasChange, dirs)
+
+	return sum(1 for c in res if c == full)
 
 free, full, floor = 0, 1, 2
-toNumbers = {'.': floor, 'L': free}
-nonFloor = []
+toNumbers = {'L' : free, '.' : floor}
 
 with open("input11.txt") as f:
-	text = f.read().splitlines()
-	rows, cols = len(text), len(text[0])
-	inp = [[floor]*(cols+2)]
-	for indexRow, row in enumerate(text, 1):
-		inp.append([floor])
-		for indexCol, col in enumerate(row, 1):
-			colValue = toNumbers[col]
-			inp[indexRow].append(colValue)
-			if colValue != floor:
-				nonFloor.append((indexRow, indexCol))
-		inp[indexRow].append(floor)
+	inp = [[floor]*128] + [[floor] + list(map(lambda x: toNumbers[x], row)) + [floor]*30 for row in f.read().splitlines()] + [[floor]*128]
 
-	inp.append([floor]*(cols+2))
+lastRow = len(inp) - 2
+lastCol = 98
+inp = [col for row in inp for col in row]
+nonFloor = [index for index, c in enumerate(inp) if c == free]
+dirs = [128, -128, 127, -127, 129, -129, 1, -1]
 
-dirs = [(1,0), (-1,0), (0,1), (0,-1), (1,1), (1,-1), (-1, 1), (-1,-1)]
-print("Star 1:",star1(inp, nonFloor, dirs))
-print("Star 2:",star2(inp, nonFloor, dirs))
+print("Star 1:", star(inp, 1, dirs))
+print("Star 2:", star(inp, 2, dirs))
