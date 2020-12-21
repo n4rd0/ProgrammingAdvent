965cec632015a823d61acf043bb29102bff99a11
usr25 <jorgespaguettini@gmail.com>
2020-12-21 11:29:15 +0100

    D21 haskell

---
+++ AoC2020/D21/usr.hs
@@ -0,0 +1,56 @@
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.List.Split (splitOn)
+import Data.List (intersperse)
+
+type Food = ([String],[String])
+
+swap (a,b) = (b,a)
+
+invert m = Map.fromListWith (++) pairs
+    where pairs = [(head vs,k) | (k,vs) <- Map.toList m]
+
+parse :: String -> Food
+parse ss = (words foods, words $ init $ filter (/=',') contains)
+    where [foods,contains] = splitOn "(contains" ss
+
+toMap :: Food -> Map.Map String [String]
+toMap (foods, allergens) = Map.fromList $ zip foods $ repeat allergens
+
+simplify :: Map.Map String [String] -> Map.Map String [String]
+simplify m = if Set.size onlyOnce == Map.size m then m else simplify m'
+    where
+        m' = Map.map aux m
+        aux ls = if length ls <= 1 then ls else filter (not . (flip Set.member) onlyOnce) ls
+        onlyOnce = foldr1 Set.union $ map Set.fromList $ Map.elems $ Map.filter ((==1).length) m
+
+genMaps :: [Food] -> (Map.Map String [String], Map.Map String String)
+genMaps ls = (allergenToFood, foodToAllergen)
+    where
+        foodToAllergen = invert allergenToFood
+        allergenToFood = simplify jointMap
+        jointMap = foldr1 (Map.unionWith aux) inverseMap
+        aux a b = Set.toList set
+            where set = (Set.fromList a) `Set.intersection` (Set.fromList b)
+        inverseMap = map (toMap . swap) ls
+
+star1 ls = length $ filter ((flip Set.member) cleanFoods) listOfIngredients
+    where
+        cleanFoods = Set.fromList $ Map.keys $ Map.filter null $ foldr1 Map.union $ map (Map.mapWithKey aux) possible
+        aux k vs = if k `Map.member` foodToAllergen
+            then [foodToAllergen Map.! k]
+            else filter (not . (flip Map.member) allergenToFood) vs
+        (allergenToFood,foodToAllergen) = genMaps ls
+        listOfIngredients = foldr1 (++) $ map fst ls
+        possible = map toMap ls
+
+star2 ls = foldr1 (++) $ intersperse "," $ map (head . snd) $ Map.toAscList allergenToFood
+    where (allergenToFood,_) = genMaps ls        
+
+main :: IO ()
+main = do
+  contents <- getContents
+  let ls = map parse $ lines contents
+
+  putStrLn $ "Star 1: " ++ (show $ star1 ls)
+  putStrLn $ "Star 2: " ++ (star2 ls)
