1b9d865ee346cc01f26de89aec4267ee9002db75
usr25 <jorgespaguettini@gmail.com>
2020-12-22 12:26:00 +0100

    D22 haskell

---
+++ AoC2020/D22/usr.hs
@@ -0,0 +1,40 @@
+import Data.List.Split (splitOn)
+import qualified Data.Set as Set
+
+score :: [Int] -> Int
+score ls = sum $ zipWith (*) (reverse ls) [1..]
+
+changeDeck :: Bool -> [Int] -> [Int] -> ([Int],[Int])
+changeDeck True (a:as) (b:bs) = (as++[a,b],bs)
+changeDeck False (a:as) (b:bs) = (as,bs++[b,a])
+
+playSubGame :: [Int] -> [Int] -> Set.Set ([Int],[Int]) -> Bool
+playSubGame pa@(a:as) pb@(b:bs) set = (pa,pb) `Set.member` set || playSubGame pa' pb' (Set.insert (pa,pb) set)
+    where (pa',pb') = changeDeck (a>b) pa pb
+playSubGame a [] _ = True
+playSubGame [] b _ = False
+
+playOneMove :: ([Int] -> [Int] -> Bool) -> [Int] -> [Int] -> ([Int],[Int])
+playOneMove determineWinner pa pb = changeDeck (determineWinner pa pb) pa pb
+
+play :: ([Int] -> [Int] -> ([Int],[Int])) -> [Int] -> [Int] -> Set.Set ([Int],[Int]) -> Int
+play _ a [] _ = score a
+play _ [] b _ = score b
+play playMove a b set = if (a,b) `Set.member` set
+    then play undefined a [] undefined
+    else play playMove a' b' ((a,b) `Set.insert` set)
+    where (a',b') = playMove a b
+
+winnerStar2 (a:as) (b:bs) = if length as >= a && length bs >= b
+    then playSubGame (take a as) (take b bs) Set.empty
+    else a > b
+
+winnerStar1 (a:_) (b:_) = a > b
+
+main :: IO ()
+main = do
+  contents <- getContents
+  let [a,b] = map (map read . tail . lines) $ splitOn "\n\n" contents :: [[Int]]
+
+  putStrLn $ "Star 1: " ++ (show $ play (playOneMove winnerStar1) a b Set.empty)
+  putStrLn $ "Star 2: " ++ (show $ play (playOneMove winnerStar2) a b Set.empty)
