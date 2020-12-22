9625e9fe14a72cbf13298438349dc2b666685740
lbm364dl <geometrydashm364@gmail.com>
2020-12-22 16:07:24 +0100

    D22 python

---
+++ AoC2020/D22/lbm.py
@@ -0,0 +1,59 @@
+from copy import deepcopy
+
+
+def parse_input():
+	with open("input22.txt") as f:
+		decks = [list(map(int, player.split('\n')[1:])) for player in f.read().strip().split('\n\n')]
+	
+	return decks
+
+
+def star_1(decks):
+	cards = deepcopy(decks)
+	while cards[0] and cards[1]:
+		player_1_card = cards[0][0]
+		player_2_card = cards[1][0]
+		cards = update_cards(cards, player_2_card > player_1_card, player_1_card, player_2_card)
+
+	return sum(card*value for card, value in zip(cards[0], range(len(cards[0]), 0, -1)))
+
+
+def star_2(decks):
+	_, winner_cards = get_winner_cards(decks, set(), set())
+	num_cards = len(winner_cards)
+	return sum(card*value for card, value in zip(winner_cards, range(num_cards, 0, -1)))
+
+
+def get_winner_cards(decks, p1_prev_configs, p2_prev_configs):
+	cards = deepcopy(decks)
+	while cards[0] and cards[1]:
+		player_1_card = cards[0][0]
+		player_2_card = cards[1][0]
+
+		if player_1_card + 1 <= len(cards[0]) and player_2_card + 1 <= len(cards[1]):
+			new_cards = [cards[0][1:player_1_card+1], cards[1][1:player_2_card+1]]
+			is_player_2_winner, _ = get_winner_cards(new_cards, deepcopy(p1_prev_configs), deepcopy(p2_prev_configs))
+			cards = update_cards(cards, is_player_2_winner, player_1_card, player_2_card)
+		else:
+			cards = update_cards(cards, player_2_card > player_1_card, player_1_card, player_2_card)
+
+		if tuple(cards[0]) in p1_prev_configs or tuple(cards[1]) in p2_prev_configs:
+			return False, cards[0]
+
+		p1_prev_configs.add(tuple(cards[0]))
+		p2_prev_configs.add(tuple(cards[1]))
+
+	is_player_2_winner = bool(cards[1])
+	return is_player_2_winner, cards[is_player_2_winner]
+
+
+def update_cards(cards, is_2_winner, player_1_card, player_2_card):
+	if not is_2_winner:
+		return [cards[0][1:] + [player_1_card, player_2_card], cards[1][1:]]
+	else:
+		return [cards[0][1:], cards[1][1:] + [player_2_card, player_1_card]]
+
+
+decks = parse_input()
+print("Star 1:", star_1(decks))
+print("Star 2:", star_2(decks))
