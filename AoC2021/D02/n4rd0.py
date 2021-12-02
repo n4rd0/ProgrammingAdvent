50ad8542cc0b8c84475e9718af360cc4fbb83786
n4rd0 <58040410+n4rd0@users.noreply.github.com>
2021-12-02 18:29:28 +0100

    Update n4rd0.py

--- AoC2021/D02/n4rd0.py
+++ AoC2021/D02/n4rd0.py
@@ -3,7 +3,6 @@ file = open("D02Input.txt","r")
 lines = file.readlines()
 file.close()
 horizontal_depth_aim = [0 for i in range(3)]
-
 for line in lines:
     number = 0
     n_moves = re.search(r'\d', line)
@@ -18,8 +17,4 @@ for line in lines:
         ##Only for fst Star
         ##horizontal_depth_aim[1] -= int(n_moves.group(0))
         horizontal_depth_aim[2] -= int(n_moves.group(0))
-    print("horiz", horizontal_depth_aim[0])
-    print("depth", horizontal_depth_aim[1])
-    print("aim", horizontal_depth_aim[2])
-
 print(horizontal_depth_aim[0],horizontal_depth_aim[1],horizontal_depth_aim[0]*horizontal_depth_aim[1])
