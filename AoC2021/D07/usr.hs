901e581a626bbd3fa1a3166f82e86877f27db8c6
usr <jorgespaguettini@gmail.com>
2021-12-07 09:52:59 +0100

    D7 usr

---
+++ AoC2021/D07/usr.hs
@@ -0,0 +1,22 @@
+import Data.List (sort)
+
+split = foldr (\c acc@(y:ys) -> if c == ',' then []:acc else (c:y):ys) [[]]
+
+triangNum n = n*(n+1) `div` 2
+
+totalFuelCost f ls pos = sum $ map (f.abs.(+(-pos))) ls
+
+--More efficient ways of computing the first star
+--First, the optimal element has to be within the list
+star1 ls = minimum $ map (totalFuelCost id ls) ls
+--Second, the optimal element is the median
+star1' ls = (totalFuelCost id ls) $ (!! (length ls `div` 2)) $ sort ls
+
+star f ls = minimum $ map (totalFuelCost f ls) [minimum ls..maximum ls]
+
+main :: IO ()
+main = do
+  contents <- getContents
+  let crabs = map read $ split contents :: [Int]
+  putStrLn $ "Star 1: " ++ (show $ star1' crabs)
+  putStrLn $ "Star 2: " ++ (show $ star triangNum crabs)
