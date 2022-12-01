a3b6c0b59ff4cb737e5b2feac436132c2220932f
n4rd0 <58040410+n4rd0@users.noreply.github.com>
2022-12-01 12:32:27 +0100

    Update D1.py

--- AoC2022/D1/D1.py
+++ AoC2022/D1/D1.py
@@ -1,34 +1,13 @@
-max = -1
-temp = 0
-with open("D1\D1.txt", 'r') as input:
-
-    for l in input.readlines():
-
-        if l != "\n":
-            temp+=int(l)
-        else: 
-            if temp > max:
-                max = temp 
-            temp = 0 
-
-print(max)
-
-temp = 0 
-max = -1
-listCal =[]
-
-with open("D1\D1.txt", 'r') as input:
-
-    for l in input.readlines():
-
-        if l != "\n":
-            temp+=int(l)
-        else: 
-            listCal.append(int(temp))
-            temp = 0 
-
-listCal.sort()
-listCal = listCal[::-1]
-print(sum(listCal[:3]))
-
-    
+m,t=0,0
+c=[]
+i=open("ProgramingAdvent\AoC2022\D1\D1.txt", 'r')
+for l in i.readlines():
+    if l!="\n":
+        t+=int(l)
+    else: 
+        c.append(int(t))
+        if t>m:
+            m=t 
+        t=0
+c.sort()
+sum(c[-3:])
